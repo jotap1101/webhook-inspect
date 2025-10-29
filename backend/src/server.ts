87233284e6207864d34b0d53372022564d57888f
jotap1101 <jotap1101.joaopedro@gmail.com>
2025-10-29 19:54:33 -0300

    refactor: update server implementation to use Fastify with ESM syntax and change port to 3333

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,7 @@
-// Require the framework and instantiate it
+import { fastify } from "fastify";
 
-// CommonJs
-const fastify = require("fastify")({
-  logger: true,
-});
+const app = fastify();
 
-// Declare a route
-fastify.get("/", function (request, reply) {
-  reply.send({ hello: "world" });
-});
-
-// Run the server!
-fastify.listen({ port: 3000 }, function (err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  // Server is now listening on ${address}
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
+  console.log("HTTP server running on http://localhost:3333");
 });
