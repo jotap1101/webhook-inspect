e2a2155bef07548ac85f8cc2eec2f6ed3f520289
jotap1101 <jotap1101.joaopedro@gmail.com>
2025-10-29 21:14:55 -0300

    feat: add drizzle configuration for PostgreSQL with DATABASE_URL

---
+++ backend/drizzle.config.ts
@@ -0,0 +1,12 @@
+import { env } from "@/env";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  out: "./src/db/migrations",
+  schema: "./src/db/schema/index.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  casing: "snake_case",
+});
