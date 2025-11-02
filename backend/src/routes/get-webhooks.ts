5bb781b278eb69ee00175e3e620c8f24b3abe927
jotap1101 <jotap1101.joaopedro@gmail.com>
2025-11-02 09:02:32 -0300

    feat: refine getWebhooksRoute response structure and improve cursor handling

--- backend/src/routes/get-webhooks.ts
+++ backend/src/routes/get-webhooks.ts
@@ -20,21 +20,21 @@ export const getWebhooksRoute: FastifyPluginAsyncZod = async (app) => {
         }),
         response: {
           200: z.object({
+            nextCursor: z.string().nullable(),
             webhooks: z.array(
               createSelectSchema(webhooksTable).pick({
                 id: true,
                 method: true,
                 pathname: true,
-                ip: true,
-                contentType: true,
-                contentLength: true,
-                queryParams: true,
-                headers: true,
-                body: true,
+                // ip: true,
+                // contentType: true,
+                // contentLength: true,
+                // queryParams: true,
+                // headers: true,
+                // body: true,
                 createdAt: true,
               })
             ),
-            nextCursor: z.string().optional(),
           }),
         },
       },
@@ -42,14 +42,25 @@ export const getWebhooksRoute: FastifyPluginAsyncZod = async (app) => {
     async (request, reply) => {
       const { limit, cursor } = request.query;
       const result = await db
-        .select()
+        .select({
+          id: webhooksTable.id,
+          method: webhooksTable.method,
+          pathname: webhooksTable.pathname,
+          // ip: webhooksTable.ip,
+          // contentType: webhooksTable.contentType,
+          // contentLength: webhooksTable.contentLength,
+          // queryParams: webhooksTable.queryParams,
+          // headers: webhooksTable.headers,
+          // body: webhooksTable.body,
+          createdAt: webhooksTable.createdAt,
+        })
         .from(webhooksTable)
         .where(cursor ? lt(webhooksTable.id, cursor) : undefined)
         .orderBy(desc(webhooksTable.id))
         .limit(limit + 1);
       const hasMore = result.length > limit;
       const webhooks = hasMore ? result.slice(0, limit) : result;
-      const nextCursor = hasMore ? webhooks[webhooks.length - 1].id : undefined;
+      const nextCursor = hasMore ? webhooks[webhooks.length - 1].id : null;
 
       return reply.status(200).send({
         webhooks,
