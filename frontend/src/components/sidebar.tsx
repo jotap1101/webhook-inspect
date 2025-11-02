cbca108bdfcc7b80196cce9cf6c4fb309398be51
jotap1101 <jotap1101.joaopedro@gmail.com>
2025-11-02 14:47:15 -0300

    feat: adjust div class order for consistent layout in Sidebar component

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -5,7 +5,7 @@ import { WebhooksList } from "./webhooks-list";
 
 export function Sidebar() {
   return (
-    <div className="h-screen flex-col">
+    <div className="flex h-screen flex-col">
       <div className="flex items-center justify-between border-b border-zinc-700 px-4 py-5">
         <div className="flex items-baseline">
           <span className="font-semibold text-zinc-100">webhook</span>
